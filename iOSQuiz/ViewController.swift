4ccd5761f9d20f71b84e9363b76d1cae360fdf4d
Safa Falaqi <sfalaqi@gmail.com>
2021-11-27 17:07:21 +0300

    Initial Commit

---
+++ iOSQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSQuiz
+//
+//  Created by Safa Falaqi on 22/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
